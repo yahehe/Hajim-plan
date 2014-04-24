48946417c179fed4a17b743908f895ba55df38d3
Thomas Tavolara <ttavolar@u.rochester.edu>
2014-04-24 20:08:33 +0000

    more fixing

--- from_aws/schedule_tom.php
+++ from_aws/schedule_tom.php
@@ -1,41 +1,36 @@
 <!DOCTYPE html>
-<html lang="en" ng-app="scheduleApp">
+<html lang="en">
 <head>
-	  	
-  	<title>ScheduleUR</title>
- 	<meta charset="utf-8">
-  	<meta name="author" content="SqlThePrql">
-	<meta name="description" content="A schedule for Hajim students.">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
- 
+  <meta charset="utf-8">
+  <title>ScheduleUR</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-	<!-- css -->
-	<link href="layoutit/src/css/bootstrap.css" rel="stylesheet">	
-	<link href="layoutit/src/css/bootstrap.min.css" rel="stylesheet">
-	<link href="layoutit/src/css/style.css" rel="stylesheet">
-
-
-	<!-- icons -->
-	<link href="img/favicon.ico" rel="icon" type="image/x-icon" />
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="layoutit/src/img/apple-touch-icon-144-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="layoutit/src/img/apple-touch-icon-114-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="layoutit/src/img/apple-touch-icon-72-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" href="layoutit/src/img/apple-touch-icon-57-precomposed.png">
-	<link rel="shortcut icon" href="layoutit/src/img/favicon.png">
-	  
-	<!-- javascript -->
-	<script type="text/javascript" src="layoutit/src/js/jquery.min.js"></script>
-	<script type="text/javascript" src="layoutit/src/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="layoutit/src/js/scripts.js"></script>
-
-  	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-  	<!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-  	<![endif]-->
 	<!--link rel="stylesheet/less" href="less/bootstrap.less" type="text/css" /-->
 	<!--link rel="stylesheet/less" href="less/responsive.less" type="text/css" /-->
 	<!--script src="js/less-1.3.3.min.js"></script-->
 	<!--append ‘#!watch’ to the browser URL, then refresh the page. -->
+
+	<link href="layoutit/src/css/bootstrap.css" rel="stylesheet">	
+	<link href="layoutit/src/css/bootstrap.min.css" rel="stylesheet">
+	<link href="layoutit/src/css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+
+  <!-- Fav and touch icons -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="layoutit/src/img/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="layoutit/src/img/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="layoutit/src/img/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="layoutit/src/img/apple-touch-icon-57-precomposed.png">
+  <link rel="shortcut icon" href="layoutit/src/img/favicon.png">
+  
+	<script type="text/javascript" src="layoutit/src/js/jquery.min.js"></script>
+	<script type="text/javascript" src="layoutit/src/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="layoutit/src/js/scripts.js"></script>
 
 <script>
 function allowDrop(ev)
