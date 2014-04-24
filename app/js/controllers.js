30e6e0ff5344830b194002d46a42842449e207f5
jeremywrnr <jeremy.warner@rochester.edu>
2014-04-24 11:30:05 -0400

    dropdown select working with nginit, combine controllers working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,7 @@
 /* Controllers */
 
 var currentMajor = 'ME';
+function updateMajor(newMajor) { currentMajor = newMajor; }
 var scheduleControllers = angular.module('scheduleControllers', []);
 
 scheduleControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
@@ -11,19 +12,19 @@ scheduleControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
     $scope.orderProp = 'age';
   }]);
 
-scheduleControllers.controller('majorCtrl', ['$scope', '$http', 
-  function($scope, $http) {
-  	$http.get('reqs/majors.json').success(function(data) {$scope.majors = data;}  ); 
-    function updateMajor(newMajor) { currentMajor = newMajor; }
-	
-}]);
 
 scheduleControllers.controller('scheduleCtrl', ['$scope', '$http', 
   function($scope, $http) {
-  	//if($scope.majors === null) $scope.majors = 'ECE'; 
+    //if($scope.majors === null) $scope.majors = 'ECE'; 
   
-  	var reqsUrl = 'reqs/' + currentMajor + '.json';
+    var reqsUrl = 'reqs/' + currentMajor + '.json';
+  	
+    $http.get('reqs/majors.json').success(function(data) {
+      $scope.majors = data;
+    }); 
+    
   	$http.get(reqsUrl).success(function(data) {
   		$scope.reqs = data;
-  	}); 	
+  	});
+
 }]);
