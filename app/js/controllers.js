cb6d31a8299ef3bf096b85d5aae67d3858d70ae2
jeremywrnr <jeremy.warner@rochester.edu>
2014-04-27 23:25:57 -0400

    fix up classes for drop down

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,44 +2,22 @@
 
 /* Controllers */
 
-function updateMajor(newMajor) { currentMajor = newMajor; };
 var scheduleControllers = angular.module('scheduleControllers', []);
+function updateMajor(newMajor) { currentMajor = newMajor; };
 
-scheduleControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
+
+scheduleControllers.controller('PhoneListCtrl', ['$scope', 'Major',
+  function($scope, Major) {
+    $scope.phones = Major.query();
     $scope.orderProp = 'age';
   }]);
 
-
 scheduleControllers.controller('scheduleCtrl', ['$scope', '$http', 
   function($scope, $http) {
-   
-
-  $http.get('reqs/majors.json').success(function(data) {
-    
-    $scope.iter = 0;
-    $scope.majors = data;
-    $scope.reqsUrl = new Array();
-  
-    for (var i = 0; $scope.majors.length > i; i++) {
-        $scope.iter = i;
-        $scope.reqsUrl[i] = 'reqs/' + $scope.majors[i].name +'.json';
-        console.log('i = ' + i  + ' '   + $scope.reqsUrl[i] + ' : success');
-    };
-   
-
-  }); 
-
-  for (var i = 0; $scope.iter >= i; i++) {
-    console.log('i = ' + i   );
-  };
- 
- //$http.get($scope.reqsUrl[0]).success(function(data) { 
-        //console.log('i = ' + i   );
-        //console.log('i = ' + i  + ' '   + $scope.reqsUrl[i] + ' : success'  );
-        //  $scope.reqs[$scope.majors[i].name] = data;         
-  //});
-
-
-}]); //controller end
+    $http.get('reqs/ECE.json').success(function(data) {
+      $scope.reqs = data;
+    });
+    $http.get('reqs/majors.json').success(function(data) {
+      $scope.majors = data;
+    }); 
+}]);
